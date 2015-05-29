a9bf96845055da717ce76d7a56c1ccdf1593c707
VVS70 <vvs70@mail.ru>
2015-05-29 22:07:15 +0300

    some change

--- app/src/main/java/ru/andro1d/circlecrop/app/MainActivity.java
+++ app/src/main/java/ru/andro1d/circlecrop/app/MainActivity.java
@@ -2,11 +2,20 @@ package ru.andro1d.circlecrop.app;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.graphics.Point;
@@ -14,7 +23,14 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
@@ -27,6 +43,10 @@ public class MainActivity extends Activity {
     private String filePath = "";
     private static Context context;
 
+    public interface OnBackPressedListener {
+        public void onBackPressed();
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,12 +58,9 @@ public class MainActivity extends Activity {
             FragmentTransaction ft = getFragmentManager().beginTransaction();
             ft.setCustomAnimations(R.animator.show_frame, R.animator.hide_frame);
             ft.add(R.id.container, aapFrag);
-            //ft.addToBackStack("app_move");
             ft.commit();
-
             ActionBar actionBar = getActionBar();
             actionBar.hide();
-
             needSplash = false;
         }
     }
@@ -64,19 +81,44 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
             switch (item.getItemId()) {
                 case R.id.action_camera: {
-                    Toast.makeText(this, "CAMERA", Toast.LENGTH_LONG).show();
+                    try {
+                        Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        File photo = null;
+                        try {
+                            photo = SplashScreen.createImageFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            photo = null; SplashScreen.photoPath = null;
+                        }
+                        captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+                        startActivityForResult(captureIntent, TAKE_PHOTO);
+                    } catch (Exception e) {
+                        String errorMessage = "Camera don't work:(";
+                        Toast toast = Toast.makeText(MainActivity.getAppContext(),
+                                        errorMessage, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
                     break;
                 }
                 case R.id.action_folder: {
-                    Toast.makeText(this, "Gallery", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, SELECT_PICTURE_ACTIVITY_REQUEST_CODE);
                     break;
                 }
                 case R.id.action_save: {
-                    Toast.makeText(this, "Save", Toast.LENGTH_LONG).show();
+                    Fragment fr = getFragmentManager().findFragmentById(R.id.container);
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.addToBackStack("app_move");
+                    ft.hide(fr);
+                    fr = new SaveResult();
+                    ft.setCustomAnimations(R.animator.show_frame, R.animator.hide_frame);
+                    ft.add(R.id.container, fr);
+                    ft.commit();
                     break;
                 }
                 case R.id.action_settings: {
-                    Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "under construction", Toast.LENGTH_LONG).show();
                     break;
                 }
 
@@ -85,14 +127,13 @@ public class MainActivity extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     public String getFilePath() {
         if (filePath==""){
-    SharedPreferences sPref;
-    sPref = getPreferences(MODE_PRIVATE);
+            SharedPreferences sPref;
+            sPref = getPreferences(MODE_PRIVATE);
         return sPref.getString("#FileName", "");
         } else {
             return filePath;
@@ -108,6 +149,55 @@ public class MainActivity extends Activity {
         ed.commit();
     }
 
-    public static Context getAppContext() {return MainActivity.context; }
+    public static Context getAppContext() {return MainActivity.context;    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case SELECT_PICTURE_ACTIVITY_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = MainActivity.getAppContext().getContentResolver()
+                              .query(selectedImage, filePathColumn, null, null, null);
+                    if (cursor.moveToFirst()) {
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        setFilePath(cursor.getString(columnIndex));
+                        AppView fr = (AppView) getFragmentManager().findFragmentById(R.id.container);
+                        fr.updateView();
+                    }
+                    cursor.close();
+                }
+                break;
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    if (SplashScreen.photoPath != null) {
+                        setFilePath(SplashScreen.photoPath);
+                        AppView fr = (AppView) getFragmentManager().findFragmentById(R.id.container);
+                        fr.updateView();
+                        SplashScreen.galleryAddPic(SplashScreen.photoPath);
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Fragment fr = getFragmentManager().findFragmentById(R.id.container);
+        OnBackPressedListener backPressedListener = null;
+
+            if (fr instanceof  OnBackPressedListener) {
+                backPressedListener = (OnBackPressedListener) fr;
+            }
+        if (backPressedListener != null) {
+            backPressedListener.onBackPressed();
+            super.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
